400989538a6f0fe0352d8070004a7d69bf54f051
Thang <91139472+putaa1@users.noreply.github.com>
2024-06-20 22:53:17 +0700

    complete

---
+++ ThangNPHE151263_Lab2/DataAccessObjects/AccountDAO.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObjects
+{
+    public class AccountDAO
+    {
+        public static AccountMember GetAccountById(string accountID)
+        {
+            var myContextDB = new MyStoreContext();
+            return myContextDB.AccountMembers.FirstOrDefault(i => i.MemberId.Equals(accountID));
+        }
+    }
+}
